c2ce7472fd6be6e44e4cf9a76b0c792554e3a2c3
Evgeniy Skripkaru <skripkaru@ddplanet.ru>
2023-03-16 18:24:02 +0300

    Add mainSlider

--- src/js/modules/slider/mainSlider.js
+++ src/js/modules/slider/mainSlider.js
@@ -1,13 +1,11 @@
-export default class Slider {
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
   constructor(page, btns) {
-    this.page = document.querySelector(page)
-    this.slides = this.page.children
-    this.btns = document.querySelectorAll(btns)
-    this.slideIndex = 1
+    super(page, btns)
   }
 
-  // n - Направление слайдера
-  // Если +1 то вперед, если -1 то назад
+  // n - Направление слайдера. Если +1 то вперед, если -1 то назад
   showSlides(n) {
     // Если n больше чем общее количество слайдов, тогда возвращаемся на 1 слайд
     if(n > this.slides.length) {
@@ -60,7 +58,7 @@ export default class Slider {
         this.plusSlides(1)
       })
 
-      btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+      btn.parentNode.previousSibling.addEventListener('click', (e) => {
         e.preventDefault()
         this.slideIndex = 1
         this.showSlides(this.slideIndex)
